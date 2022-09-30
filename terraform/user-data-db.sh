96bf3a3141e7bc7783c795805e9babd62e544842
marco-fpereira <marcoantonio_fp@usp.br>
2022-09-29 23:11:06 -0300

    fixing user data shell script

--- terraform/user-data-db.sh
+++ terraform/user-data-db.sh
@@ -15,7 +15,7 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 chmod a+r /etc/apt/keyrings/docker.gpg
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 service docker start
 docker run hello-world
 
